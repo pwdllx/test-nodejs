230cfee9da856fabf8ef636b729e70f58055ff4a
Feret-Jocelin <feret.jocelin@outlook.fr>
2023-11-17 16:17:38 +0100

    delete log

--- src/controller/brand/put.ts
+++ src/controller/brand/put.ts
@@ -10,8 +10,7 @@ export async function addBrand(
 ): Promise<void> {
   try {
     const brand = new Brand(req.body.title);
-    const brandRepo = new BrandRepository();console.log('in post route !!!!!!!!!');
-    
+    const brandRepo = new BrandRepository();
     const data = await brandRepo.create(brand);
     res.json(data);
   } catch (error) {
